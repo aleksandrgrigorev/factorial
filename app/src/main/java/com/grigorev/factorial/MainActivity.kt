c55b6c4efcefe0e6e5127c07f62fd94adbaf60f7
aleksandrgrigorev <xundr1992@gmail.com>
2023-10-25 16:16:41 +0400

    init commit

---
+++ app/src/main/java/com/grigorev/factorial/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.grigorev.factorial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.grigorev.factorial.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
